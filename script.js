c5a7d8777a1792297a0d48672686c33a157811cf
Ujjawal7906 <ujjawalthakur896@gmail.com>
2025-10-05 20:47:25 +0530

    Update

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed("#element", {
+    strings: [
+        "Web Developer",
+        "Web Designer",
+        "UI Designer",
+        "Graphic Designer",
+   ],
+    typeSpeed: 50,
+   });
